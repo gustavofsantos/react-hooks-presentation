5d94f5cb725689c3b35a484b285e1cc3561717da
Gustavo Santos <gustavo@send4.com.br>
2019-05-20 11:41:28 -0300

    improvements

---
+++ slides/13.js
@@ -0,0 +1,20 @@
+import { useState, useCallback } from 'react';
+
+/**
+ *
+ * @param {object} props useTodo Custom Hook Properties
+ * @param {string[]?} props.INITIAL_STATE Todos initial state;
+ */
+export default function useTodos(props) {
+  const [todos, setTodos] = useState(props.INITIAL_STATE || []);
+
+  const addTodo = useCallback((todo) => {
+    setTodos([...todos, todo]);
+  }, [todos]);
+
+  const removeTodo = useCallback((todo) => {
+    setTodos(todos.filter(item => item !== todo));
+  }, [todos]);
+
+  return { todos, addTodo, removeTodo };
+}
